eeffc9d79c27d97afdcb3740cd961d6f48e857c4
teru <you@example.com>
2025-04-15 21:37:18 +0900

    Footerコンポーネントを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,7 @@ export default function RootLayout({
         <div className="flex flex-col min-h-screen">
           <Header /> 
           {children}
-          <footer>footer</footer>
+          <Footer /> 
         </div>
       </body>
     </html>
